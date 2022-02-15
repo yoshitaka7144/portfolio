96192d7714dd2b7da8ef4df371194d4013075ee7
yoshitaka7144 <yoshitaka7144@gmail.com>
2022-02-15 23:16:15 +0900

    スムーススクロールをjsで処理するように変更。プロフィール要素の高さ修正。ヘッダーリンクのデザイン修正。

--- js/headerMenu.js
+++ js/headerMenu.js
@@ -1,6 +1,25 @@
 // ヘッダーメニュー用js
 window.onload = function () {
 
+  // ページ内リンク遷移処理
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+  const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
+  const header = document.querySelector('#header');
+  anchorLinksArr.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const targetId = link.hash;
+      const targetElement = document.querySelector(targetId);
+      const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top;
+      const headerHeight = header.offsetHeight;
+      const totalScrollAmount = targetOffsetTop - headerHeight;
+      window.scrollTo({
+        top: totalScrollAmount,
+        behavior: "smooth"
+      });
+    });
+  });
+
   // トップへ戻るボタンの表示処理
   if (window.scrollY >= 100) {
     showBackBtn();
